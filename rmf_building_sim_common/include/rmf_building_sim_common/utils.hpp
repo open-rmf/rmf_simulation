375479003aa314ba30f833fb965447856756fc3d
Luca Della Vedova <luca@openrobotics.org>
2021-04-05 11:22:07 +0800

    Refactor and cleanup utils
    Signed-off-by: Luca Della Vedova <luca@openrobotics.org>

--- rmf_building_sim_common/include/rmf_building_sim_common/utils.hpp
+++ rmf_building_sim_common/include/rmf_building_sim_common/utils.hpp
@@ -6,16 +6,6 @@
 
 namespace rmf_building_sim_common {
 
-// TODO(MXG): Refactor the use of this function to replace it with
-// compute_desired_rate_of_change()
-double compute_ds(
-  double s_target,
-  double v_actual,
-  double v_max,
-  double accel_nom,
-  double accel_max,
-  double dt);
-
 struct MotionParams
 {
   double v_max = 0.2;
@@ -26,98 +16,22 @@ struct MotionParams
 };
 
 //==============================================================================
+// TODO(MXG): Refactor the use of this function to replace it with
+// compute_desired_rate_of_change()
 double compute_ds(
   double s_target,
   double v_actual,
   const double v_max,
   const double accel_nom,
   const double accel_max,
-  const double dt)
-{
-  double sign = 1.0;
-  if (s_target < 0.0)
-  {
-    // Limits get confusing when we need to go backwards, so we'll flip signs
-    // here so that we pretend the target is forwards
-    s_target *= -1.0;
-    v_actual *= -1.0;
-    sign = -1.0;
-  }
-
-  // We should try not to shoot past the targstd::vector<event::ConnectionPtr> connections;et
-  double next_s = s_target / dt;
-
-  // Test velocity limit
-  next_s = std::min(next_s, v_max);
-
-  // Test acceleration limit
-  next_s = std::min(next_s, accel_nom * dt + v_actual);
-
-  if (v_actual > 0.0 && s_target > 0.0)
-  {
-    // This is what our deceleration should be if we want to begin a constant
-    // deceleration from now until we reach the goal
-    double deceleration = pow(v_actual, 2) / s_target;
-    deceleration = std::min(deceleration, accel_max);
-
-    if (accel_nom <= deceleration)
-    {
-      // If the smallest constant deceleration for reaching the goal is
-      // greater than
-      next_s = -deceleration * dt + v_actual;
-    }
-  }
-
-  // Flip the sign the to correct direction before returning the value
-  return sign * next_s;
-}
+  const double dt);
 
 //==============================================================================
 double compute_desired_rate_of_change(
-  double _s_target,
-  double _v_actual,
-  const MotionParams& _motion_params,
-  const double _dt)
-{
-  double sign = 1.0;
-  if (_s_target < 0.0)
-  {
-    // Limits get confusing when we need to go backwards, so we'll flip signs
-    // here so that we pretend the target is forwards
-    _s_target *= -1.0;
-    _v_actual *= -1.0;
-    sign = -1.0;
-  }
-
-  // We should try not to shoot past the target
-  double v_next = _s_target / _dt;
-
-  // Test velocity limit
-  v_next = std::min(v_next, _motion_params.v_max);
-
-  // Test acceleration limit
-  v_next = std::min(v_next, _motion_params.a_nom * _dt + _v_actual);
-
-  if (_v_actual > 0.0 && _s_target > 0.0)
-  {
-    // This is what our deceleration should be if we want to begin a constant
-    // deceleration from now until we reach the goal
-    double deceleration = pow(_v_actual, 2) / (2.0 * _s_target);
-    deceleration = std::min(deceleration, _motion_params.a_max);
-
-    if (_motion_params.a_nom <= deceleration)
-    {
-      // If the smallest constant deceleration for reaching the goal is
-      // greater than the nominal acceleration, then we should begin
-      // decelerating right away so that we can smoothly reach the goal while
-      // decelerating as close to the nominal acceleration as possible.
-      v_next = -deceleration * _dt + _v_actual;
-    }
-  }
-
-  // Flip the sign the to correct direction before returning the value
-  return sign * v_next;
-}
+    double _s_target,
+    double _v_actual,
+    const MotionParams& _motion_params,
+    const double _dt);
 
 //==============================================================================
 template<typename SdfPtrT, typename SdfElementPtrT>
@@ -136,20 +50,7 @@ bool get_element_required(
   return true;
 }
 
-/*
-double compute_desired_rate_of_change(
-    double _s_target,
-    double _v_actual,
-    const MotionParams& _motion_params,
-    const double _dt);
-
-bool get_element_required(
-    const sdf::ElementPtr& _sdf,
-    const std::string& _element_name,
-    sdf::ElementPtr& _element);
-*/
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 template<typename T, typename SdfPtrT>
 bool get_sdf_attribute_required(SdfPtrT& sdf, const std::string& attribute_name,
   T& value)
@@ -177,7 +78,7 @@ bool get_sdf_attribute_required(SdfPtrT& sdf, const std::string& attribute_name,
   return false;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 template<typename T, typename SdfPtrT>
 bool get_sdf_param_required(SdfPtrT& sdf, const std::string& parameter_name,
   T& value)
@@ -204,7 +105,7 @@ bool get_sdf_param_required(SdfPtrT& sdf, const std::string& parameter_name,
   return false;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+//==============================================================================
 template<typename T, typename SdfPtrT>
 void get_sdf_param_if_available(SdfPtrT& sdf, const std::string& parameter_name,
   T& value)
