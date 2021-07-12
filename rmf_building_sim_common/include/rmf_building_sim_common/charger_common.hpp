5880631b9dbff1f2b6ff06067c1de5d5829e2891
Arjo Chakravarty <arjo@openrobotics.org>
2021-07-12 16:59:56 +0800

    initial charger library
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

---
+++ rmf_building_sim_common/include/rmf_building_sim_common/charger_common.hpp
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef RMF_BUILDING_SIM_COMMON__CHARGER_COMMON_HPP
+#define RMF_BUILDING_SIM_COMMON__CHARGER_COMMON_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <rmf_charger_msgs/msg/charger_state.hpp>
+#include <rmf_charger_msgs/msg/charger_request.hpp>
+#include <rmf_charger_msgs/msg/charger_cancel.hpp>
+
+namespace rmf_building_sim_common {
+
+class ChargerCommon
+{
+public:
+  ChargerCommon(std::shared_ptr<rclcpp::Node> node, 
+    rclcpp::QoS qos);
+
+  void add_charger(std::string name, double x, double y, std::string level);
+
+  void idle(std::string name);
+
+  void update_charger_charge(std::string name, rclcpp::Duration charge);
+
+  void complete_charger_charge(std::string name);
+
+  void start_charging(std::string name);
+
+private:
+  rclcpp::Logger logger() const
+  {
+    return rclcpp::get_logger("charger_stub");
+  }
+  void on_charger_request_recieved(
+    const rmf_charger_msgs::msg::ChargerRequest::SharedPtr request);
+
+  void on_charger_request_cancelled(
+    const rmf_charger_msgs::msg::ChargerCancel::SharedPtr cancel);
+
+  void status_publisher_timer_callback();
+
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::Publisher<rmf_charger_msgs::msg::ChargerState>::SharedPtr
+    state_publisher_;
+
+  rclcpp::Subscription<rmf_charger_msgs::msg::ChargerRequest>::SharedPtr
+    request_subscription_;
+  rclcpp::Subscription<rmf_charger_msgs::msg::ChargerCancel>::SharedPtr
+    cancel_subscription_;
+
+  rclcpp::TimerBase::SharedPtr status_publisher_timer_;
+
+  enum State
+  {
+    IDLE = rmf_charger_msgs::msg::ChargerState::CHARGER_IDLE,
+    ASSIGNED = rmf_charger_msgs::msg::ChargerState::CHARGER_ASSIGNED, 
+    CHARGING = rmf_charger_msgs::msg::ChargerState::CHARGER_CHARGING, 
+    COMPLETED = rmf_charger_msgs::msg::ChargerState::CHARGER_COMPLETED
+  };
+
+  struct Charger
+  {
+    std::string name_;
+    double x_;
+    double y_;
+    std::string level_;
+    State state_;
+    std::string assigned_robot_name_;
+    std::string assigned_robot_fleet_;
+    rclcpp::Duration charge_time_;
+    int curr_req_ = 0;
+  };
+  std::unordered_map<std::string, Charger> chargers_;
+  int req_id_ = 0;
+  std::mutex mtx_;
+
+};
+
+}  // namespace rmf_robot_sim_common
+
+#endif /* RMF_BUILDING_SIM_COMMON__CHARGER_COMMON_HPP */
---
+++ rmf_building_sim_common/src/charger_common.cpp
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#include <rmf_building_sim_common/charger_common.hpp>
+#include <functional>
+
+namespace rmf_building_sim_common {
+
+ChargerCommon::ChargerCommon(
+  std::shared_ptr<rclcpp::Node> node,
+  rclcpp::QoS qos):
+  node_(node)
+{
+  state_publisher_ =
+    node_->create_publisher<rmf_charger_msgs::msg::ChargerState>(
+      "rmf_charger/state", 1);
+  request_subscription_ =
+    node_->create_subscription<rmf_charger_msgs::msg::ChargerRequest>(
+      "rmf_charger/requests",
+      qos,
+      std::bind(
+        &ChargerCommon::on_charger_request_recieved,
+        this,
+        std::placeholders::_1));
+  cancel_subscription_ =
+    node_->create_subscription<rmf_charger_msgs::msg::ChargerCancel>(
+      "rmf_charger/cancel",
+      qos,
+      std::bind(
+        &ChargerCommon::on_charger_request_cancelled,
+        this,
+        std::placeholders::_1));
+
+  status_publisher_timer_ = node_->create_wall_timer(
+    std::chrono::milliseconds(500),
+    std::bind(&ChargerCommon::status_publisher_timer_callback, this));
+}
+
+void ChargerCommon::add_charger(
+  std::string name,
+  double x,
+  double y,
+  std::string level)
+{
+  Charger charger {name,x,y,level,State::IDLE,"", "", rclcpp::Duration(0)};
+  chargers_.insert({name, charger});
+}
+
+void ChargerCommon::update_charger_charge(
+  std::string name, rclcpp::Duration charge)
+{
+  auto it = chargers_.find(name);
+  if(it == chargers_.end()) return;
+  {
+    std::lock_guard<std::mutex> lock(mtx_);
+    it->second.state_ = State::CHARGING;
+    it->second.charge_time_ = charge;
+  }
+}
+
+void ChargerCommon::on_charger_request_recieved(
+  const rmf_charger_msgs::msg::ChargerRequest::SharedPtr request)
+{
+  std::lock_guard<std::mutex> lock(mtx_);
+  auto it = chargers_.find(request->charger_name);
+  if(it == chargers_.end())
+  {
+    RCLCPP_ERROR(logger(),
+      "Charger not found: Recieved request with invalid charger name");
+    return;
+  }
+
+  if(it->second.state_ != State::IDLE)
+  {
+    RCLCPP_ERROR(logger(),
+      "Charger %s is busy", request->charger_name.c_str());
+  }
+  it->second.state_ = State::ASSIGNED;
+  it->second.assigned_robot_fleet_ = request->fleet_name;
+  it->second.assigned_robot_name_ = request->robot_name;
+  it->second.curr_req_ = req_id_++;
+}
+
+void ChargerCommon::complete_charger_charge(std::string name)
+{
+  std::lock_guard<std::mutex> lock(mtx_);
+  auto it = chargers_.find(name);
+  if(it == chargers_.end()) return;
+  {
+    it->second.state_ = State::COMPLETED;
+  }
+}
+
+void ChargerCommon::status_publisher_timer_callback()
+{
+  std::lock_guard<std::mutex> lock(mtx_);
+  for(auto& [name, charger]: chargers_)
+  {
+    rmf_charger_msgs::msg::ChargerState msg;
+    {
+      msg.charger_name = name;
+      msg.state = charger.state_;
+      msg.request_id = charger.curr_req_;
+      msg.robot_fleet = charger.assigned_robot_fleet_;
+      msg.robot_name = charger.assigned_robot_name_;
+    }
+    state_publisher_->publish(msg);
+  }
+}
+
+}
