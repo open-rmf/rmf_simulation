bf4c1164ffec99ab08e8bfb55254d4db31320489
Aaron Chong <aaronchongth@gmail.com>
2024-09-23 22:17:03 +0800

    Stop using deprecated gz math API
    Signed-off-by: Aaron Chong <aaronchongth@gmail.com>

--- rmf_building_sim_gz_plugins/src/crowd_simulator.cpp
+++ rmf_building_sim_gz_plugins/src/crowd_simulator.cpp
@@ -542,7 +542,8 @@ void CrowdSimulatorPlugin::_update_internal_object(
     convert_to_ign_math_pose_3d<gz::math::Pose3d>();
   gz::math::Pose3d agent_pose = get_agent_pose(obj_ptr->agent_ptr,
       delta_sim_time);
-  agent_pose += initial_pose;
+  agent_pose.CoordPositionAdd(initial_pose.Pos());
+  agent_pose.CoordRotationAdd(initial_pose.Rot());
 
   // get components to be updated
   auto traj_pose_comp =
